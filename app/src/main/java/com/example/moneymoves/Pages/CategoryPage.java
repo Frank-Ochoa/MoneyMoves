d6745cec9ad2edb00e661b25b332c87721496ccf
Frank-Ochoa <35553286+Frank-Ochoa@users.noreply.github.com>
2019-12-09 17:43:45 -0800

    Fix and Merge

--- app/src/main/java/com/example/moneymoves/Pages/CategoryPage.java
+++ app/src/main/java/com/example/moneymoves/Pages/CategoryPage.java
@@ -2,10 +2,12 @@ package com.example.moneymoves.Pages;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
@@ -18,15 +20,19 @@ import com.example.moneymoves.Adapters.CategoryAdapter;
 import com.example.moneymoves.Database.Entities.MonthlySpent;
 import com.example.moneymoves.R;
 import com.example.moneymoves.ViewModels.SpentPageViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class CategoryPage extends AppCompatActivity
 {
 
+	public static final int ADD_NOTE_REQUEST = 1;
+	public static final int EDIT_NOTE_REQUEST = 2;
 	public static final String EXTRA_ID = "ID";
 	public static final String EXTRA_CATEGORY = "CATEGORY";
 	public static final String EXTRA_AMOUNT = "AMOUNT";
+	public static String category;
 
 	CategoryAdapter adapter;
 	private SpentPageViewModel spentViewModel;
@@ -37,6 +43,16 @@ public class CategoryPage extends AppCompatActivity
 		setContentView(R.layout.activity_category_page);
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+
+		FloatingActionButton addSpentButton = findViewById(R.id.addSpentButton);
+		addSpentButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override public void onClick(View view)
+			{
+				Intent intent = new Intent(CategoryPage.this, AddSpentPage.class);
+				startActivityForResult(intent, ADD_NOTE_REQUEST);
+			}
+		});
 
 		final TextView x = findViewById(R.id.noteAmount);
 		TextView y = findViewById(R.id.budgetCategory);
@@ -92,11 +108,55 @@ public class CategoryPage extends AppCompatActivity
 				{
 					x.setText(0.0 + "/" + budgetAmount);
 				}
-				else {
+				else
+				{
 					x.setText(aDouble + "/" + budgetAmount);
 				}
 			}
 		});
 	}
 
+	@Override protected void onActivityResult(int requestCode, int resultCode,
+			@Nullable Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK)
+		{
+			assert data != null;
+			String note = data.getStringExtra(AddSpentPage.EXTRA_NOTE);
+			double amount = data.getDoubleExtra(AddSpentPage.EXTRA_AMOUNT, 0.0);
+			MonthlySpent spentItem = new MonthlySpent(category, note, amount);
+			spentViewModel.insertNote(spentItem);
+
+			Toast.makeText(this, "New Note Saved", Toast.LENGTH_SHORT).show();
+		}
+		else if (requestCode == EDIT_NOTE_REQUEST && resultCode == RESULT_OK)
+		{
+			int id = data.getIntExtra(AddSpentPage.EXTRA_ID, -1);
+			if (id == -1)
+			{
+				Toast.makeText(this, "Update Failed", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			String note = data.getStringExtra(AddSpentPage.EXTRA_NOTE);
+			double amount = data.getDoubleExtra(AddExpensePage.EXTRA_AMOUNT, 0.0);
+			MonthlySpent spentItem = new MonthlySpent(category, note, amount);
+			spentItem.setId(id);
+			spentViewModel.updateNote(spentItem);
+
+			Toast.makeText(this, "Note Updated", Toast.LENGTH_SHORT).show();
+		}
+		else
+		{
+			Toast.makeText(this, "Budget Not Saved", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	public void mainPage(View view)
+	{
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+
 }
+
